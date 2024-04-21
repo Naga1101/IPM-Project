66dc299e4fe710425f3b464fc1cb5cc9f96edf76
Pedro Meruge <a100709@alunos.uminho.pt>
2024-04-21 22:20:08 +0100

    Fixing popup sair

--- ESIdeal/src/components/PopExit.vue
+++ ESIdeal/src/components/PopExit.vue
@@ -2,25 +2,37 @@
 
 <template>
   <transition name="dialog-fade">
-    <div class="dialog-backdrop">  
-      <dialog class="exit-popup" @click="close">
+    <div class="dialog-backdrop" @click="close">  
+      <dialog class="exit-popup" >
         <div class="faixa-vermelha"></div>
         <div class="mensagem">
             <div class="imgAviso">
-              <img :src="aviso" alt="triangle">
+              <img src="/svgs/danger_symbol_grey.svg" alt="simbolo aviso"/>
             </div>
             <div class="msg">
-              <p>Tem a certeza que pretende sair?</p>
-              <p>Todas as ações em progresso seram suspendidas</p>
+              <div class="upper-text">
+                <span>Tem a certeza que pretende sair?</span>
+              </div>
+              <div v-if="servicoADecorrer" class="lower-text">
+                <span  class="lower-title">
+                  Ações em progresso seram suspendidas
+                </span>
+                <p class="lower-servico">
+                  <b>Serviço: {{ servicoADecorrer.def_servico.descricao}} (#{{  servicoADecorrer.id }})</b>
+                </p>
+                <p class="lower-veiculo">
+                  <b>Veículo: {{ servicoADecorrer.id_veiculo }}</b> 
+                </p>
+              </div>
             </div>
           </div>
         <div class="linha"></div>
         <div class="butoes">
           <div class="imgRec" @click="close">
-              <img :src="recusar" alt="rec-but">
+              <img src="/svgs/botao_recusar_popup.svg" alt="recusar"/>
             </div>
             <div class="imgConf" @click="logout">
-              <img :src="confirmar" alt="conf-but">
+              <img src="/svgs/botao_confirmar_popup.svg" alt="confirmar"/>
             </div>
         </div>
       </dialog>
@@ -29,35 +41,54 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
-
-const aviso = '/images/aviso.png';
-const recusar = '/images/recusar.png';
-const confirmar = '/images/confirmar.png';
+  import {serviceState} from '../scripts/stores.js';
+  import * as Consts from '../models/consts.js';
 </script>
 
 <script>
     export default {
       name: 'PopExit',
+      data() {
+        return {
+          servicoADecorrer: null
+        }
+      },
       methods: {
+
         goToPage(pageUrl) {
             this.$router.push(pageUrl);
         },
-        logout() {
+
+        async logout() {
+          if (this.servicoADecorrer) { // se houver serviço a decorrer
+            const result = await DBRequests.postSuspendedService(this.message,this.currentService.id)
+            if (result) {
+              //parar serviço a decorrer no registo de estado
+              dbData.clearOnGoingService();
+              dbData.updateServiceState(this.currentService.id, Consts.EstadoServico.PARADO);
+            }
+        }
           this.goToPage("/login");
         },
+
         close() {
           this.$emit('close');
         },
       },
+
+      created() {
+        const state = serviceState();
+        this.servicoADecorrer = state.onGoingService
+        console.log(this.servicoADecorrer)
+      }
     }
 </script>
 
 <style scoped>
 .exit-popup {
   position: fixed;
-  width: 551px;
-  height: 269px;
+  width: 50vw;
+  height: fit-content;
   border-radius: 15px;
   background-color: #FFFFFF;
   border: 2px solid #DC564E;
@@ -68,48 +99,63 @@ const confirmar = '/images/confirmar.png';
 }
 
 .faixa-vermelha {
-  height: 42px;
+  height: 50px;
   background-color: #DC564E;
   border-radius: 10px 10px 0 0;
 }
 
 .mensagem {
-  height: 138px;
+  height: 80%;
   display: flex;
   flex-direction: row;
   align-items: center;
-  padding-right: 0%;
+  padding: 15px 20px;
 }
 
 .imgAviso {
-  width: 20%; 
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
 .imgAviso img {
-  max-width: 100%;
-  padding-left: 10%;
+  width: calc(max(90%,60px));
+  height: auto;
 }
 
-.mensagem .msg {
-  margin-left: 30px;
-  flex: 1;
+.msg {
+  margin-left: 20px;
+  flex: 3;
+  display: flex;
+  flex-direction: column;
 }
 
-.mensagem .msg p:nth-child(1) {
+.upper-text {
+  margin-bottom: 15px;
   color: #000000;
-  font-size: 25px;
-  margin-top: 30px;
-  margin-bottom: 5px;
-  font-family: 'Lato', sans-serif;
+  font-size: 1.5em;
 }
 
-.mensagem .msg p:nth-child(2) {
-  color: #727272;
-  font-size: 20px;
-  text-align: center;
-  font-family: 'Lato', sans-serif;
-  margin-bottom: 40px;
-  margin-right: 9%;
+.lower-text {
+  font-size: 1.2em;
+  color: var(--text-light-grey);
+  justify-content: left;
+  line-height: 25px;
+}
+
+.lower-text b{
+  font-weight: 600;
+}
+.lower-servico, .lower-veiculo, .lower-title {
+  text-align: left;
+  margin: 0
+}
+
+.lower-title {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 10px;
 }
 
 .linha {
@@ -125,34 +171,25 @@ const confirmar = '/images/confirmar.png';
   justify-content: space-between;
 }
 
-.imgRec {
-  width: 20%; 
+.butoes img {
+  width: calc(max(90%,40px));
+  height: auto;
+  display: flex;
+  justify-content: space-between;
+
 }
 
-.imgRec img {
-  max-width: 68px;
-  max-height: 63px;
-  padding-left: 0;
-}
-
-.imgRec :hover{
-  cursor: pointer;
-}
-
-.imgConf {
-  width: 20%; 
-}
-
-.imgConf :hover{
+.imgRec, .imgConf {
   cursor: pointer
 }
 
-.imgConf img {
-  max-width: 68px;
-  max-height: 63px;
-  padding-left: 35%;
+.imgRec:hover, .imgConf:hover {
+  opacity: 50%;
 }
 
+.butoes {
+  padding: 5px 20px;
+}
 .dialog-fade-enter, .dialog-fade-leave-to {
   opacity: 0;
 }
